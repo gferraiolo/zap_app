1b31e2c46762e2c239e58fefb8805ead304051d3
Guy Ferraiolo <gdf@norstriliasoftware.com>
2011-03-25 19:42:43 -0400

    Initial commit (gdf)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZapApp::Application.config.secret_token = '3a75d89a758fee4ac17b05f1d228dd2b1aa3b5edc5e50f0e7ee3f92f8f57c003214c3c5cbd5673bc315973d3effd0b0d3ac360c59dbffaea01a36c8306fcec64'
